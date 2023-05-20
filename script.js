c65d36a8b56cba4443d195ba45a677f6afa7c75e
dkrishna11 <116519809+dkrishna11@users.noreply.github.com>
2023-05-20 23:42:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function getFormvalue() {
     //Write your code here
 	let type=document.getElementById("first");
 	let last=document.getElementById("last");
-	alert(type.innerText+" "+last.InnerText);
+	alert(type.value+" "+last.value);
 
 }
